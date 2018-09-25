1ef4a43718afc6515d483e0549dc5fff74c91078
Matt Foster <matt@fostermatt.com>
2018-09-25 16:58:28 -0700

    more standard library stuff

--- Closures.playground/Pages/Challenges.xcplaygroundpage/Contents.swift
+++ Closures.playground/Pages/Challenges.xcplaygroundpage/Contents.swift
@@ -48,7 +48,11 @@ let numbers = [1,2,3,4,5]
 
 let numberStrings = numbers.map { String($0) }
 
+// using-flat-map
 
+let numbers2 = [[1,2,3], [4,5,6], [7,8,9]]
+
+let flattenedNumbers = numbers2.flatMap { $0 }
 
 
 
--- Closures.playground/Pages/Standard Library Functions.xcplaygroundpage/Contents.swift
+++ Closures.playground/Pages/Standard Library Functions.xcplaygroundpage/Contents.swift
@@ -42,19 +42,47 @@ let dateStrings = ["20 Oct 1978", "11 Jan 1947", "28 Mar 2002"]
 let dates = dateStrings.customMap { Formatter.date(from: $0) }
 print(dates)
 
+// Flat Map
 
+struct Post {
+	var content: String
+	var tags: [String]
+}
 
+let blog = [
+	Post(content: "Hello, world", tags: ["first", "programming"]),
+	Post(content: "Just another short post", tags: ["general"])
+]
 
+let tags = blog.flatMap { $0.tags }
 
+extension Array {
+	func customFlatMap<T>(_ transform: (Element) -> [T]) -> [T] {
+		var result = [T]()
+		
+		for x in self {
+			result.append(contentsOf: transform(x))
+		}
+		
+		return result
+	}
+}
 
+struct Account {
+	let username: String
+	let billingAddress: String?
+}
 
+let allUsers = [
+	Account(username: "pasanpr", billingAddress: nil),
+	Account(username: "benjakuben", billingAddress: "1234 Imaginary Street"),
+	Account(username: "instantNadel", billingAddress: "5678 Doesn't Live Here Dr."),
+	Account(username: "sketings", billingAddress: nil),
+	Account(username: "paradoxed", billingAddress: "1122 Nope Lane")
+]
 
-
-
-
-
-
-
+let validAddresses = allUsers.flatMap { $0.billingAddress }
+validAddresses.count
 
 
 
